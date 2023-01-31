f166a36bfa8e55e3de4f035867eec9f42504b99d
Diana Lin <linjdiana@gmail.com>
2023-01-30 21:45:43 -0600

    hmm

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
 // Code EyesOnMe Component Here
+    import React from 'react'
+
+    export default function EyesOnMe() {
+      return (
+        <button onFocus={() => console.log("Good!")} onBlur={() => console.log("Hey! Eyes on me!")}>EyesOnMe</button>
+      )
+    }
